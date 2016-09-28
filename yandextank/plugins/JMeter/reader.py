Fixed negative latency with connect timeout, added receive_time calculation

yandextank/plugins/JMeter/reader.py
@@ -94,6 +94,16 @@ jtl_types = {
     'connect_time': np.float64,
 }
 
+def fix_latency(row):
+    if row['latency'] < row['connect_time']:
+        if row['interval_real'] < row['connect_time']:
+            latency = 0
+        else:
+            latency = row['interval_real'] - row['connect_time']
+    else:
+        latency = row['latency'] - row['connect_time']
+    return latency
+
 
 # timeStamp,elapsed,label,responseCode,success,bytes,grpThreads,allThreads,Latency
 def string_to_df(data):
@@ -108,9 +118,10 @@ def string_to_df(data):
     chunk.set_index(['receive_sec'], inplace=True)
     l = len(chunk)
     chunk['connect_time'] = (chunk['connect_time'].fillna(0) * 1000).astype(np.int64)
-    chunk['latency'] = chunk['latency']  * 1000 - chunk['connect_time']
+    chunk['latency'] = chunk['latency'] * 1000
+    chunk['latency'] = chunk.apply(fix_latency, axis=1)
     chunk['send_time'] = np.zeros(l)
-    chunk['receive_time'] = np.zeros(l)
+    chunk['receive_time'] = chunk['interval_real'] - chunk['latency'] - chunk['connect_time']
     chunk['interval_event'] = np.zeros(l)
     chunk['size_out'] = np.zeros(l)
     chunk['net_code'] = exc_to_net(chunk['retcode'], chunk['success'])